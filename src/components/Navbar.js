dd23dc84c1cd444a586e7e7fe73d03ae18527e48
Jatin <jatindahiya100@gmail.com>
2022-11-17 16:54:52 +0530

    Commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,8 +3,19 @@ import logo from "./img/jatindahiya.png";
 import "./css/Navbar.css"
 
 export default function Navbar(props) {
+
+  const toggleMenu = () => {
+    var menu = document.getElementsByClassName("nav-menu")[0];
+    if (menu.style.display === "none" || menu.style.display === "") {
+      menu.style.display = "inline-flex";
+    } else {
+      menu.style.display = "none";
+    }
+  }
+
   return (
     <div className={`navbar bg-${props.theme}`}>
+
       <img className='logo' src={logo} alt="" />
 
       <ul className="nav-menu">
@@ -14,8 +25,18 @@ export default function Navbar(props) {
         <li><a className={`text-${props.theme === 'light' ? 'dark' : 'light'}`} href="/">Chat <ion-icon name="chatbubbles"></ion-icon></a></li>
       </ul>
 
-      <div onClick={props.toggleMode} className={`toggle-theme-btn bg-${props.theme === 'light' ? 'dark' : 'light'} text-${props.theme === 'light' ? 'light' : 'dark'}`}>
-        <ion-icon name="sunny-outline"></ion-icon>
+
+
+      <div className='nav-right-items'>
+
+        <div onClick={toggleMenu} className="expand-menu">
+          <ion-icon name="menu"></ion-icon>
+        </div>
+
+        <div onClick={props.toggleMode} className={`toggle-theme-btn bg-${props.theme === 'light' ? 'dark' : 'light'} text-${props.theme === 'light' ? 'light' : 'dark'}`}>
+          <ion-icon name="sunny-outline"></ion-icon>
+        </div>
+
       </div>
     </div>
   )
