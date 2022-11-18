e683bc0e1c57a486242350c3791c38b09dd0c036
Jatin <jatindahiya100@gmail.com>
2022-11-18 16:02:34 +0530

    Commit

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,22 @@ import React, { useState } from 'react';
 import Home from './Home';
 
 function App() {
-  const [defaultTheme, setTheme] = useState("light");
+
+  const detectCurrentTheme = () => {
+    var setTheme;
+    const theme = () => window.matchMedia("(prefers-color-scheme: dark)").matches;
+    if (theme === false) {
+      setTheme = 'dark'
+    } else {
+      setTheme = 'light';
+    }
+    return setTheme;
+  }
+
+
+
+  var detectTheme = detectCurrentTheme();
+  const [defaultTheme, setTheme] = useState(detectTheme);
   // Dark Mode / Light Mode Function
   const toggleMode = () => {
     if (defaultTheme === 'light') {
