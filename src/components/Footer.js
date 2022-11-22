0308a75bffb5c26c72eda5491257617096dcd17d
Jatin <jatindahiya100@gmail.com>
2022-11-22 14:34:09 +0530

    Commit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -24,9 +24,9 @@ export default function Footer(props) {
 
       <ul>
         <label htmlFor="footer-title">Social Links</label>
-        <li><a className={`text-${props.theme === 'light' ? 'dark' : 'light'}`} href="https://github.com/jatindahiya100?tab=repositories" target="_blank">Github <ion-icon name="logo-github"></ion-icon></a></li>
-        <li><a className={`text-${props.theme === 'light' ? 'dark' : 'light'}`} href="https://www.linkedin.com/in/jatin-dahiya-325b93200/" target="_blank">Linkedin <ion-icon name="logo-linkedin"></ion-icon></a></li>
-        {/* <li><a className={`text-${props.theme === 'light' ? 'dark' : 'light'}`} href="/">Whatsapp <ion-icon name="logo-whatsapp"></ion-icon></a></li> */}
+        <li><a className={`text-${props.theme === 'light' ? 'dark' : 'light'}`} href="https://github.com/jatindahiya100?tab=repositories" rel="noreferrer">Github <ion-icon name="logo-github"></ion-icon></a></li>
+        <li><a className={`text-${props.theme === 'light' ? 'dark' : 'light'}`} href="https://www.linkedin.com/in/jatin-dahiya-325b93200/" rel="noreferrer">Linkedin <ion-icon name="logo-linkedin"></ion-icon></a></li>
+        <li><a className={`text-${props.theme === 'light' ? 'dark' : 'light'}`} href="https://wa.me/919485901109?text=[Hello Jatin, I Want to connect with you. This message is sent through your website link]">Whatsapp <ion-icon name="logo-whatsapp"></ion-icon></a></li>
         <li><a className={`text-${props.theme === 'light' ? 'dark' : 'light'}`} href="mailto:jatindahiya100@gmail.com">Email <ion-icon name="logo-google"></ion-icon></a></li>
       </ul>
     </footer>
