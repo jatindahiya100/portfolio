f87efebcd2accf704bbe52c086c41993bd00e5ec
Jatin <jatindahiya100@gmail.com>
2022-11-22 15:16:19 +0530

    Commit

--- src/components/Work.js
+++ src/components/Work.js
@@ -5,6 +5,7 @@ import Rainfire from './img/Rainfire.png'
 import Backlinks from './img/Backlinks-Tracker.png'
 import JPLewis from './img/JP-Lewis-Group.png'
 import pizzaBook from './img/PizzaBook-Home.png'
+import customDashboardUI from './img/customDashboardUI.png'
 import Appointmentwale from './img/Appointmentwale.png'
 
 export default function Work(props) {
@@ -14,7 +15,7 @@ export default function Work(props) {
     theme: props.theme,
     h1: '1). Rainfire.in',
     span: 'Online Store',
-    description: 'Project Summary: Rainfire.in is an online selling store designed & developed by me for my non-Upwork client. This a full stack project. built from scratch with HTML, CSS, Javascript, jQuery, and PHP with AJAX and MYSQL.',
+    description: 'Project Summary: Rainfire.in is an online selling store designed & developed by me for my non-Upwork client. This a full stack project built from scratch with HTML, CSS, Javascript, jQuery, and PHP with AJAX and MYSQL.',
     btnText: 'Visit Site',
     btnIcon: 'link',
     linkTo: 'http://rainfire.in/',
@@ -24,7 +25,7 @@ export default function Work(props) {
     theme: props.theme,
     h1: '2). JP LEWIS GROUP, LLC',
     span: 'Landing Page',
-    description: 'Designed, Developed, and hosted a landing page for an international cleint  "James Lewis Group LLC"',
+    description: 'Designed, Developed, and hosted a landing page for an international cleint:  "James Lewis Group LLC". The deliverables were a simple and elegant landing page built with pure HTML, CSS, Javascript.',
     btnText: 'Visit Site',
     btnIcon: 'link',
     linkTo: 'https://jplewisgroup.com/',
@@ -51,9 +52,21 @@ export default function Work(props) {
     linkTo: 'https://github.com/jatindahiya100/pizzaBook',
     imgSrc: pizzaBook
   }
+
+  let customDahboardObj = {
+    theme: props.theme,
+    h1: '5). Custom Dashboard UI',
+    span: 'Upwork',
+    description: 'Client Name: Faizy Jamal. Location: United States (Richmond). Project Summary: Designed and Developed a user-friendly, responsive and eye astounding custom dashboard for his website',
+    btnText: 'Source Code',
+    btnIcon: 'logo-github',
+    linkTo: 'https://github.com/jatindahiya100/appointment',
+    imgSrc: customDashboardUI
+  }
+
   let appointmentwaleObj = {
     theme: props.theme,
-    h1: '5). AppointmentWale',
+    h1: '6). E-Appointment',
     span: 'Booking System',
     description: 'A web portal for salons where their visitors can have schedule appointments and track its live status so that they can visit the respective salon on time',
     btnText: 'Source Code',
@@ -71,6 +84,7 @@ export default function Work(props) {
       <Template myObj={jpLewisGroupObj} />
       <Template myObj={backlinksObj} />
       <Template myObj={pizzabookObj} />
+      <Template myObj={customDahboardObj} />
       <Template myObj={appointmentwaleObj} />
     </>
   )
