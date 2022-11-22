1539b7088319a62d20baf30dea6bb995d9df1fdc
Jatin <jatindahiya100@gmail.com>
2022-11-22 12:00:06 +0530

    Commit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,7 +8,7 @@ export default function Footer(props) {
     <footer className={`bg-${props.theme}`}>
       <ul>
         <label htmlFor="footer-title">Quick Links</label>
-        <li><Link className={`text-${props.theme === 'light' ? 'dark' : 'light'}`} href="/portfolio-react.js">Home <ion-icon name="home"></ion-icon></Link></li>
+        <li><Link className={`text-${props.theme === 'light' ? 'dark' : 'light'}`} to="/portfolio-react.js">Home <ion-icon name="home"></ion-icon></Link></li>
         <li><a className={`text-${props.theme === 'light' ? 'dark' : 'light'}`} href="https://www.upwork.com/workwith/jatindahiya">Hire Me <ion-icon name="ribbon"></ion-icon></a></li>
         <li><Link className={`text-${props.theme === 'light' ? 'dark' : 'light'}`} to="/work">Work <ion-icon name="logo-react"></ion-icon></Link></li>
         <li><a className={`text-${props.theme === 'light' ? 'dark' : 'light'}`} href="mailto:jatindahiya100@gmail.com">Contact <ion-icon name="chatbubbles"></ion-icon></a></li>
