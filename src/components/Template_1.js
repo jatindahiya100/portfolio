29ecfcdeb7464f450161ddcbef3fbb884ed639a2
Jatin <jatindahiya100@gmail.com>
2022-12-09 15:11:54 +0530

    Commit

--- src/components/Template_1.js
+++ src/components/Template_1.js
@@ -13,7 +13,7 @@ export default function Template_1(props) {
     };
     return (
         <div className={`grid bg-${props.myObj.theme}`}>
-            <div data-aos="fade-up" data-aos-anchor-placement="bottom-bottom" className='grid-item'>
+            <div data-aos="fade-up" data-aos-once="true" data-aos-anchor-placement="bottom-bottom" className='grid-item'>
                 <h1 className={`text-${props.myObj.theme === 'light' ? 'dark' : 'light'}`}>{props.myObj.h1} <span> {props.myObj.span} </span></h1>
                 <p className={`text-xtra-${props.myObj.theme === 'light' ? 'light' : 'dark'}`}>{props.myObj.description}
                 </p>
