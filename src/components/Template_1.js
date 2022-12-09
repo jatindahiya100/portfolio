aa06380a5f34f045695b005de2f1b9a0119a8b3a
Jatin <jatindahiya100@gmail.com>
2022-12-09 15:33:49 +0530

    Commit

--- src/components/Template_1.js
+++ src/components/Template_1.js
@@ -14,10 +14,10 @@ export default function Template_1(props) {
     return (
         <div className={`grid bg-${props.myObj.theme}`}>
             <div className='grid-item'>
-                <h1 data-aos="fade-up" data-aos-anchor-placement="bottom-bottom" className={`text-${props.myObj.theme === 'light' ? 'dark' : 'light'}`}>{props.myObj.h1} <span> {props.myObj.span} </span></h1>
-                <p data-aos="fade-up" data-aos-anchor-placement="bottom-bottom" className={`text-xtra-${props.myObj.theme === 'light' ? 'light' : 'dark'}`}>{props.myObj.description}
+                <h1 data-aos="fade-up" data-aos-anchor-placement="top-bottom" className={`text-${props.myObj.theme === 'light' ? 'dark' : 'light'}`}>{props.myObj.h1} <span> {props.myObj.span} </span></h1>
+                <p data-aos="fade-up" data-aos-anchor-placement="top-bottom" className={`text-xtra-${props.myObj.theme === 'light' ? 'light' : 'dark'}`}>{props.myObj.description}
                 </p>
-                <div data-aos="fade-up" data-aos-anchor-placement="bottom-bottom" className="social-links">
+                <div data-aos="fade-up" data-aos-anchor-placement="top-bottom" className="social-links">
                     {
                         props.myObj.skills.map((skills, i) => {
                             return <Link className={`text-${props.myObj.theme === 'light' ? 'dark' : 'light'}`} key={i}>{skills}</Link>
@@ -25,10 +25,10 @@ export default function Template_1(props) {
                     }
 
                 </div>
-                <button data-aos="fade-up" data-aos-anchor-placement="bottom-bottom" onClick={handleClick}>{props.myObj.btnText} <ion-icon name={props.myObj.btnIcon}></ion-icon></button>
+                <button data-aos="fade-up" data-aos-anchor-placement="top-bottom" onClick={handleClick}>{props.myObj.btnText} <ion-icon name={props.myObj.btnIcon}></ion-icon></button>
             </div>
             <div className='grid-item'>
-                <img  data-aos="fade-up" data-aos-anchor-placement="bottom-bottom" src={props.myObj.imgSrc} alt="" />
+                <img  data-aos="fade-up" data-aos-anchor-placement="top-bottom" src={props.myObj.imgSrc} alt="" />
             </div>
         </div>
     )
