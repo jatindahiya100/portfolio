481327d6b751430d250f9288af01d101245c5df0
Jatin <jatindahiya100@gmail.com>
2022-12-10 15:50:44 +0530

    Commit

--- src/components/Hire.js
+++ src/components/Hire.js
@@ -7,8 +7,8 @@ import launch from './img/launch-amico.svg'
 export default function Hire(props) {
     let hireObj = {
         theme: props.theme,
-        h1: 'Designing Next-Level',
-        span: 'Company Websites',
+        h1: 'Get me @',
+        span: '$10/hour',
         description: 'Web design is not about making a website—it’s about making an investment in your organization. Successful web design takes into account more than just aesthetics… It’s a reflection of you, your company, and your vision.',
         skills: ["Web Design", "Web Development", "Hosting", "Domain Registration", "Wordpress", "Shopify", "Full Stack Development", "HTML", "CSS", "Javascript", "PHP", "MYSQL"],
         btnText: 'Hire Me On Upwork',
