31a49afec5d29b8e63f98b857491e4e118e18be4
Jatin <jatindahiya100@gmail.com>
2022-12-13 16:26:48 +0530

    Commit

--- src/components/Hire.js
+++ src/components/Hire.js
@@ -10,7 +10,7 @@ export default function Hire(props) {
         h1: 'Get me @',
         span: '$10/hour',
         description: 'Web design is not about making a website—it’s about making an investment in your organization. Successful web design takes into account more than just aesthetics… It’s a reflection of you, your company, and your vision.',
-        skills: ["Web Design", "Web Development", "Hosting", "Domain Registration", "Wordpress", "Shopify", "Full Stack Development", "HTML", "CSS", "Javascript", "PHP", "MYSQL"],
+        skills: ["HTML", "CSS", "Javascript", "PHP", "MYSQL","React.js","Web Design", "Web Development", "Hosting", "Domain Registration", "Wordpress", "Shopify", "Full Stack Development"],
         btnText: 'Hire Me On Upwork',
         btnIcon: 'link',
         linkTo: 'https://www.upwork.com/freelancers/~012d6eb926a1f7a86a',
