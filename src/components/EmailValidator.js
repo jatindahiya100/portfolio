b67b7eb8c3cf76e1b1a5132232d9995dc7fa433e
Jatin <jatindahiya100@gmail.com>
2022-12-14 01:02:40 +0530

    Commit

---
+++ src/components/EmailValidator.js
@@ -0,0 +1,98 @@
+import React from 'react'
+import './css/Template_1.css'
+import './css/EmailValidator.css'
+import email_validation from './img/email_validation_api.png'
+// setTimeout(() => {
+//     fetch('https://api.emailvalidation.io/v1/info?email=support@emailvalidation.io', {
+//         headers: {
+//             apikey: 'pmxiHTqsgugAZHe8H7tiQr3Dzw9ZZrv7RgRH78cf'
+//         }
+//     })
+//         .then(response => response.json())
+//         .then(data => {
+//             console.log(data);
+//         });
+// }, 3000);
+
+var arr = {
+    "email": "support@emailvalidation.io",
+    "user": "support",
+    "domain": "emailvalidation.io",
+    "smtp_check": true,
+    "mx_found": true,
+    "did_you_mean": "",
+    "role": true,
+    "disposable": false,
+    "score": 0.64,
+    "free": false,
+    "format_valid": true,
+    "catch_all": true
+}
+
+console.log(arr["email"]);
+
+export default function EmailValidator(props) {
+
+    return (
+        <div className={`EmailValidator grid center-grid-item bg-${props.theme}`}>
+            <div className="grid-item">
+                <h1 className={`text-${props.theme === 'light' ? 'dark' : 'light'}`}>Email Validation Tool <span>Try it yourself</span></h1>
+                <form>
+                    <input className={`text-${props.theme === 'light' ? 'dark' : 'light'}`} type="email" placeholder='Email Address' />
+                    <button>Validate Email</button>
+                </form>
+                <div className={`response bg-${props.theme === 'light' ? 'light' : 'slight-dark'}`}>
+                    <table>
+                        <tbody className={`text-${props.theme === 'light' ? 'dark' : 'light'}`}>
+                            <tr>
+                                <td>Email</td>
+                                <td>
+                                    {arr["email"]}<ion-icon name={`${arr["smtp_check"] === true ? 'checkmark-circle-outline' : 'close-circle'}`}></ion-icon>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>Username</td>
+                                <td>{arr["user"]}</td>
+                            </tr>
+                            <tr>
+                                <td>domain</td>
+                                <td>{arr["domain"]}</td>
+                            </tr>
+                            <tr>
+                                <td>SMTP Check</td>
+                                <td>
+                                    <ion-icon name={`${arr["smtp_check"] === true ? 'checkmark-circle-outline' : 'close-circle'}`}></ion-icon>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>disposable</td>
+                                <td>
+                                    <ion-icon name={`${arr["disposable"] === true ? 'checkmark-circle-outline' : 'close-circle'}`}></ion-icon>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>Free</td>
+                                <td>
+                                    <ion-icon name={`${arr["free"] === true ? 'checkmark-circle-outline' : 'close-circle'}`}></ion-icon>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>Valid Format</td>
+                                <td>
+                                    <ion-icon name={`${arr["format_valid"] === true ? 'checkmark-circle-outline' : 'close-circle'}`}></ion-icon>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+
+            <div className="grid-item center-grid-item">
+                <figure>
+                    <img src={email_validation} alt="" />
+                </figure>
+            </div>
+
+        </div>
+    )
+}
--- src/components/Tools.js
+++ src/components/Tools.js
@@ -1,21 +1,11 @@
 import React from 'react'
-import Template from './Template_1';
-import productNameGenerator from './img/productnamegenerator.svg'
+import EmailValidator from './EmailValidator';
 
 export default function Tools(props) {
-  let hireObj = {
-    theme: props.theme,
-    h1: 'AI Based Product Name Generator Powered By',
-    span: 'Open AI',
-    description: "Create product names from examples words. Product description: A home milkshake maker Seed words: fast, healthy, compact. Product names: HomeShaker, Fit Shaker, QuickShake, Shake Maker",
-    skills: ["Open AI API", "Product Name Generator", "Engine: text-davinci-003"],
-    btnText: 'Launching Soon',
-    btnIcon: 'eye',
-    imgSrc: productNameGenerator
-  }
+ 
   return (
     <>
-      <Template myObj={hireObj} />
+      <EmailValidator theme={props.theme} />
     </>
   )
 }
