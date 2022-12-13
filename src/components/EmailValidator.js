104021a25b55a13c753a5cc572b87154275d255e
Jatin <jatindahiya100@gmail.com>
2022-12-14 02:02:09 +0530

    Commit

--- src/components/EmailValidator.js
+++ src/components/EmailValidator.js
@@ -2,28 +2,36 @@ import React from 'react'
 import './css/Template_1.css'
 import './css/EmailValidator.css'
 import email_validation from './img/email_validation_api.png'
+// setTimeout(() => {
+//     fetch('https://api.emailvalidation.io/v1/info?email=support@emailvalidation.io', {
+//         headers: {
+//             apikey: 'process.env.REACT_APP_API_KEY'
+//         }
+//     })
+//         .then(response => response.json())
+//         .then(data => {
+//             console.log(data);
+//         });
+// }, 3000);
 
+var arr = {
+    "email": "support@emailvalidation.io",
+    "user": "support",
+    "domain": "emailvalidation.io",
+    "smtp_check": true,
+    "mx_found": true,
+    "did_you_mean": "",
+    "role": true,
+    "disposable": false,
+    "score": 0.64,
+    "free": false,
+    "format_valid": true,
+    "catch_all": true
+}
 
-
-
+console.log(arr["email"]);
 
 export default function EmailValidator(props) {
-    const API_KEY = process.env.REACT_APP_API_KEY;
-    console.log(API_KEY)
-
-    const ValidateEmail = (e) => {
-        e.preventDefault();
-
-        // fetch('https://api.emailvalidation.io/v1/info?email=support@emailvalidation.io', {
-        //     headers: {
-        //         apikey: process.env.REACT_APP_API_KEY,
-        //     }
-        // })
-        //     .then(response => response.json())
-        //     .then(data => {
-        //         console.log(data);
-        //     });
-    }
 
     return (
         <div className={`EmailValidator grid center-grid-item bg-${props.theme}`}>
@@ -31,10 +39,51 @@ export default function EmailValidator(props) {
                 <h1 className={`text-${props.theme === 'light' ? 'dark' : 'light'}`}>Email Validation Tool <span>Try it yourself</span></h1>
                 <form>
                     <input className={`text-${props.theme === 'light' ? 'dark' : 'light'}`} type="email" placeholder='Email Address' />
-                    <button onClick={ValidateEmail}>Validate Email</button>
+                    <button>Validate Email</button>
                 </form>
                 <div className={`response bg-${props.theme === 'light' ? 'light' : 'slight-dark'}`}>
-
+                    <table>
+                        <tbody className={`text-${props.theme === 'light' ? 'dark' : 'light'}`}>
+                            <tr>
+                                <td>Email</td>
+                                <td>
+                                    {arr["email"]}<ion-icon name={`${arr["smtp_check"] === true ? 'checkmark-circle-outline' : 'close-circle'}`}></ion-icon>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>Username</td>
+                                <td>{arr["user"]}</td>
+                            </tr>
+                            <tr>
+                                <td>domain</td>
+                                <td>{arr["domain"]}</td>
+                            </tr>
+                            <tr>
+                                <td>SMTP Check</td>
+                                <td>
+                                    <ion-icon name={`${arr["smtp_check"] === true ? 'checkmark-circle-outline' : 'close-circle'}`}></ion-icon>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>disposable</td>
+                                <td>
+                                    <ion-icon name={`${arr["disposable"] === true ? 'checkmark-circle-outline' : 'close-circle'}`}></ion-icon>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>Free</td>
+                                <td>
+                                    <ion-icon name={`${arr["free"] === true ? 'checkmark-circle-outline' : 'close-circle'}`}></ion-icon>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>Valid Format</td>
+                                <td>
+                                    <ion-icon name={`${arr["format_valid"] === true ? 'checkmark-circle-outline' : 'close-circle'}`}></ion-icon>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
                 </div>
             </div>
 
