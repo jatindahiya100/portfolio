5976b0c09ea5191b9f6d0d73f3401be8ca7c2d63
Jatin Dahiya <jatindahiya100@gmail.com>
2023-06-01 00:18:57 +0530

    upload

--- src/pages/Hire.js
+++ src/pages/Hire.js
@@ -11,7 +11,7 @@ export default function Hire(props) {
             h1: 'Get me @',
             span: '$15/hour',
             description: 'Web design is not about making a website—it’s about making an investment in your organization. Successful web design takes into account more than just aesthetics… It’s a reflection of you, your company, and your vision.',
-            skills: ["HTML", "CSS", "Javascript", "PHP", "MYSQL", "React.js", "Web Design", "Web Development", "Hosting", "Domain Registration", "Wordpress", "Shopify", "Full Stack Development"],
+            skills: ["React.js", "Node.js", "HTML5", "CSS", "Javascript", "jQuery", "PHP", "MYSQL", "Web Design", "Web Development", "Hosting", "Domain Registration", "Wordpress", "Website Builders", "Full Stack Development"],
             btnText: 'Hire Me On Upwork',
             btnIcon: 'link',
             linkTo: 'https://www.upwork.com/freelancers/~012d6eb926a1f7a86a',
